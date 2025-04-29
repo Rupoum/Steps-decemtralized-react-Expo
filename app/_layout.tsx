3f80067e7c37b434893a6a530fd563ff6c2ed07a
Rupoum <rupoummitra15278@gmail.com>
2025-04-29 20:10:58 +0530

    badges

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -49,6 +49,10 @@ export default function RootLayout() {
         }}
       />
       <Stack.Screen name="(nonav)/profile" options={{ title: "Profile" }} />
+      <Stack.Screen
+        name="(nonav)/achievments"
+        options={{ title: "Achievments" }}
+      />
     </Stack>
   );
 }
